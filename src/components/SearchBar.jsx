eaafe6f8f765c1b90df9b167cb407fa5444b210f
manuel lantieri <manuel.lantieri02@gmail.com>
2025-02-19 09:14:05 +0100

    component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,98 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useCategoriesDataContext } from "../contexts/CategoriesDataContext";
+import axios from "axios";
 
-const SearchBar = ({ onSearch }) => {
-  const [query, setQuery] = useState("");
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
 
-  const handleSearch = () => {
-    if (query.trim() !== "") {
-      onSearch(query);
+  const { setMovies, setSeries } = useCategoriesDataContext();
+
+  //Movies
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/search/movie",
+        {
+          params: {
+            api_key: "f8a49a9cb468145b7722dd6220ba2269",
+            query: search || "star wars",
+          },
+        }
+      );
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
     }
   };
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Inserisci il titolo del film"
-      />
-      <button onClick={handleSearch}>Cerca</button>
-    </div>
-  );
-};
+  // Series
 
-export default SearchBar;
+  const fetchSeries = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/search/tv",
+        {
+          params: {
+            api_key: "f8a49a9cb468145b7722dd6220ba2269",
+            query: search || "star wars",
+          },
+        }
+      );
+      setSeries(response.data.results);
+    } catch (error) {
+      console.error("Error fetching series:", error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Get movies
+
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "f8a49a9cb468145b7722dd6220ba2269",
+          language: "it-IT",
+          query: search,
+        },
+      })
+      .then((res) => setMovies(res.data.results));
+
+    // Get Series
+    axios
+      .get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: "f8a49a9cb468145b7722dd6220ba2269",
+          language: "it-IT",
+          query: search,
+        },
+      })
+      .then((res) => setSeries(res.data.results));
+  };
+
+  useEffect(() => {
+    fetchSeries();
+    fetchMovies();
+  }, []);
+
+  return (
+    <form onSubmit={handleSearch} className="flex gap-3.5">
+      <input
+        name="search"
+        type="search"
+        placeholder="Cerca..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="bg-white p-1.5 rounded-2xl"
+      />
+      <button
+        className="bg-red-600 px-3.5 py-1 text-white font-medium rounded-2xl cursor-pointer"
+        type="submit"
+      >
+        Cerca
+      </button>
+    </form>
+  );
+}
---
+++ src/components/Text.jsx
@@ -0,0 +1,13 @@
+export default function Message() {
+  return (
+    <span>
+      const message = ` Non perderti la magia dei film! 🎬 Scopri migliaia di
+      film e serie disponibile SOLO su BoolFlix. Iscriviti ora per divertirti
+      di: ✔️ Accesso illimitato ai film e alle serie più popolari ✔️ Consigli
+      personalizzati solo per te ✔️ Streaming di qualità HD e Ultra HD, nessuna
+      pubblicità 👉 Prova GRATIS per i primi 7 giorni! 🎥No lascia che
+      l'intrattenimento ti aspetti. BoolFlix è il tuo mondo di film, su a
+      portata di clic.
+    </span>
+  );
+}
